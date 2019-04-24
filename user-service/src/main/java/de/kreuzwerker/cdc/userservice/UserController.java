45038a0b0716cfebd8da8173c483041f7714eab9
ram.mandavkar <ram.mandavkar@PUNITP338968D.ad.infosys.com>
2019-04-24 14:55:35 +0530

    For verify 1

--- user-service/src/main/java/de/kreuzwerker/cdc/userservice/UserController.java
+++ user-service/src/main/java/de/kreuzwerker/cdc/userservice/UserController.java
@@ -1,8 +1,14 @@
 package de.kreuzwerker.cdc.userservice;
 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import au.com.dius.pact.model.ProviderState;
 
 @RestController
 public class UserController {
@@ -16,6 +22,11 @@ public class UserController {
     @GetMapping("/users/{userId}")
     public User getUser(@PathVariable String userId) {
         return userService.findUser(userId);
+    }
+    
+    @PostMapping("/tasks/pactStateChange")
+    public ResponseEntity<?> stateChange(@RequestBody ProviderState state) {
+   	   return ResponseEntity.ok().build();
     }
 
 }
